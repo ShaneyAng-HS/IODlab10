daf1a829dd757678d37e74d0145f9212784cb594
ShaneyAng-HS <shaney_ang@yahoo.com>
2025-04-16 16:56:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import pandas as pd
 import joblib
 from utils import preprocessor
 
+from huggingface_hub import hf_hub_download
+
+REPO_ID = ???
+FILENAME = "model_.joblib"
+
 def run():
-    model = ???
+    model_path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
+    model = joblib.load(model_path)
 
     st.title("Sentiment Analysis")
     st.text("Basic app to detect the sentiment of text.")
@@ -13,13 +19,13 @@ def run():
     st.text("")
     predicted_sentiment = ""
     if st.button("Predict"):
-        predicted_sentiment = ???
+        predicted_sentiment = model.predict(pd.Series([userinput]))
         if predicted_sentiment == 1:
             output = 'positive 👍'
         else:
             output = 'negative 👎'
         sentiment=f'Predicted sentiment of "{userinput}" is {output}.'
-        st.success(???)
+        st.success(sentiment)
 
 if __name__ == "__main__":
     run()
